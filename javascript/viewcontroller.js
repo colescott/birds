2f97ec45ff9c4120619018c090d7055618255bf1
Cole Scott <colescottsf@gmail.com>
2016-07-02 06:13:10 -0700

    Add ajax load to userauth.js and format all

--- javascript/viewcontroller.js
+++ javascript/viewcontroller.js
@@ -1,5 +1,5 @@
-$(document).keydown(function(event){
-    if(event.keyCode == 13){
+$(document).keydown(function(event) {
+    if (event.keyCode == 13) {
         $("#loginButton").click();
     }
 });
@@ -9,44 +9,52 @@ $('img').bind('contextmenu', function(e) {
 });
 
 function loadView(path) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("content").innerHTML = xhttp.responseText;
-    }
-  };
-  xhttp.open("GET", path, true);
-  xhttp.send();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("content").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", path, true);
+    xhttp.send();
+}
+
+function homePage() {
+    loadView("views/homepage.html");
+}
+
+function birdtreePage() {
+    loadView("views/birdtree.html");
 }
 
 function registerPage() {
-  loadView("views/register.html");
+    loadView("views/register.html");
 }
 
 function forgotPasswordPage() {
-  loadView("views/forgotpassword.html");
+    loadView("views/forgotpassword.html");
 }
 
 function changeEmailPage() {
-  loadView("views/changeemail.html");
+    loadView("views/changeemail.html");
 }
 
 function changePasswordPage() {
-  loadView("views/changepassword.html");
+    loadView("views/changepassword.html");
 }
 
 function deleteUserPage() {
-  loadView("views/deleteuser.html");
+    loadView("views/deleteuser.html");
 }
 
 function spoof1Page() {
-  loadView("views/spoof1.html");
+    loadView("views/spoof1.html");
 }
 
 function spoof2Page() {
-  loadView("views/spoof2.html");
+    loadView("views/spoof2.html");
 }
 
 function spoof3Page() {
-  loadView("views/spoof3.html");
+    loadView("views/spoof3.html");
 }
