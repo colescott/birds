0220f616d4db2b35b1c4d8988523016ed382b41e
Cole Scott <colescottsf@gmail.com>
2016-09-12 12:31:05 -0700

    Change protectedPaths list to authenticate middleware - change JWT to load token as Bearer

--- birds/api/v1/index.js
+++ birds/api/v1/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const jwt = require('jsonwebtoken');
-const ejwt = require('express-jwt');
+const expressJwt = require('express-jwt');
 const url  = require('url');
 const router = express.Router();
 
@@ -11,66 +11,10 @@ const User = require('./models/user');
 
 const jwtSecret = process.env.JWT_SECRET;
 
-// Paths not requiring auth. Path is string or regex
-const unprotectedPaths = [
-    {path: '/auth/login', method: 'POST'},
-    {path: '/users', method: 'POST'},
-    {path: '/users', method: 'GET'},
-    {path: /\/users\/\w+/igm, method: 'GET'}
-]
+router.use(passport.initialize());
+router.use(passport.session());
 
-const filter = (req) => {
-    var found = false;
-    unprotectedPaths.forEach( (obj) => {
-        if(req.method == obj.method)
-        {
-            if(typeof obj.path === 'string')
-            {
-                if(req.url == obj.path)
-                    found = true;
-            } else {
-                if(obj.path.test(req.url) !== null)
-                    found = true;
-            }
-        }
-    });
-    return found;
-}
-
-router.use(ejwt({secret: jwtSecret, userProperty: 'tokenPayload', getToken: function fromHeaderOrQuerystring (req) {
-    if (req.get('tokenPayload')) {
-        return JSON.parse(req.get('tokenPayload')).token;
-    } else if (req.query && req.query.token) {
-        return req.query.token;
-    }
-        return null;
-    }
-}).unless(filter));
-
-router.use(function(req, res, next) {
-    if(filter(req))
-        return next();
-    if (req.tokenPayload) {
-        getUserById(req.tokenPayload.id, (err, user) => {
-            req.user = user;
-            if (req.user) {
-                return next();
-            } else {
-                return unauthorized(res);
-            }
-        });
-    } else {
-        return noSession(res);
-    }
-});
-
-// Error handler middleware
-router.use(function(err, req, res, next) {
-    console.error(err);
-    if(err.code == "credentials_required")
-        return noSession(res);
-    return res.status(err.status || 500).send(err);
-});
+const authenticate = expressJwt({secret: jwtSecret});
 
 passport.use(new LocalStrategy((username, password, done) => {
     User.authenticate()(username, password, (err, user, passErr) => {
@@ -84,10 +28,6 @@ passport.use(new LocalStrategy((username, password, done) => {
         }
     });
 }));
-
-// use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 router.get("/ping", (req, res) => {
     return res.send("Pong v1!");
@@ -138,7 +78,7 @@ router.get("/users/:id", (req, res) => {
     });
 });
 
-router.put("/users/:id", (req, res) => {
+router.put("/users/:id", authenticate, (req, res) => {
     if(!req.isAuthenticated())
     {
         return noSession(res);
@@ -198,11 +138,7 @@ router.put("/users/:id", (req, res) => {
         });
 });
 
-router.put("/users/:id/:action", (req, res) => {
-    if(!req.isAuthenticated())
-    {
-        return noSession(res);
-    }
+router.put("/users/:id/:action", authenticate, (req, res) => {
     if(req.user.id != req.params.id)
     {
         return unauthorized(res);
@@ -225,30 +161,29 @@ router.put("/users/:id/:action", (req, res) => {
 });
 
 router.post('/auth/login', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
+    passport.authenticate('local', {
+      session: false
+    }, function(err, user, info) {
         if (err) return next(err);
         if (!user) {
             return unauthorized(res);
         } else {
             var val = {};
-            val.token = jwt.sign({id: user.id}, jwtSecret, { expiresIn: 18000 /* 60 * 60 * 5 */ });
+            val.token = jwt.sign({id: user.id}, jwtSecret, { expiresIn: 2 * 60 * 60 });
             val.user = sterilizeUser(user);
             return res.send(data(val));
         }
     })(req, res, next);
 });
 
-router.post('/auth/logout', (req, res) => {
+router.post('/auth/logout', authenticate, (req, res) => {
     if(!req.isAuthenticated())
     {
         return noSession(res);
     }
 
     req.logout();
-    req.session.destroy(function() {
-        res.clearCookie('connect.sid');
-        return res.send(data({message: "Logged out successfully"}));
-    });
+    return res.send(data({message: "Logged out successfully"}));
 });
 
 const getUserById = (id, cb) => {
