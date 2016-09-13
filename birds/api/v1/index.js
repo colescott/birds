4c8016487d9f93d41040a374a2a79b054fa32d58
Cole Scott <colescottsf@gmail.com>
2016-09-12 22:20:48 -0700

    Oh lord hear my prayer and make this work

--- birds/api/v1/index.js
+++ birds/api/v1/index.js
@@ -157,7 +157,6 @@ router.put("/users/:id/:action", authenticate, (req, res) => {
 
 router.post('/auth/login', function(req, res, next) {
     passport.authenticate('local', {
-        usernameField: 'email',
         session: false
     }, function(err, user, info) {
         if (err) return next(err);
