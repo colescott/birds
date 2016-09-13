b563d2f86fe00585000df62251f7a67d6d29cf82
Cole Scott <colescottsf@gmail.com>
2016-09-13 08:21:13 -0700

    Add supertest for api testing

--- birds/test/test.js
+++ birds/test/test.js
@@ -1,1 +1,14 @@
+const request = require('supertest');
+const app = require("../app.js");
+
 it("should run this test", () => {});
+
+describe('GET /api/v1/users', function() {
+  it('respond with json', function(done) {
+    request(app)
+      .get('/api/v1/users')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
