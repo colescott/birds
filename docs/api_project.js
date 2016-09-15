71a64082a6ee9d9bb6f3ea15ca38f9ef024a494a
Cole Scott <colescottsf@gmail.com>
2016-09-15 01:18:39 -0700

    Fix apidocs url (#42)

--- docs/api_project.js
+++ docs/api_project.js
@@ -3,12 +3,12 @@ define({
   "version": "0.1.0",
   "description": "Birds api version 1",
   "title": "Birds api v1",
-  "url": "https://team4159.github.io/birds",
+  "url": "/api/v1/",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-09-15T08:02:37.623Z",
+    "time": "2016-09-15T08:10:57.941Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
