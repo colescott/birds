d363b8bde16dcf7f90f5f84b5479f018a8dc0d4a
Cole Scott <colescottsf@gmail.com>
2016-09-15 01:07:48 -0700

    Fix apidocs version number (#41)

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "Birds api v1",
-  "version": "1.0",
+  "version": "0.1.0",
   "description": "Birds api version 1",
   "title": "Birds api v1",
   "url": "https://team4159.github.io/birds",
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-09-15T06:54:13.195Z",
+    "time": "2016-09-15T08:02:37.623Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
