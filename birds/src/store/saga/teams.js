6a98d1360c74a805e2318f82ff37bb8887220961
Elias Lawson-Fox <eliaslfox@gmail.com>
2016-09-16 02:44:32 -0700

    did stuff

--- birds/src/store/saga/teams.js
+++ birds/src/store/saga/teams.js
@@ -10,33 +10,40 @@ import * as s from "../selectors.js";
 function* teams() {
     console.log("loaded");
     while (true) {
-        const action = yield take([
-            c.JOIN_TEAM,
-            c.CREATE_TEAM
-        ]);
-        console.log("taken");
-        const { token } = yield select(s.getAuth);
-        switch (action.type) {
-            case c.JOIN_TEAM: {
-                const { number, pass } = action.payload;
-                console.log(action.payload);
-                const { id: uid } = yield select(s.getAuth);
-                yield call(api.teams.join, number, pass, uid, token);
-                const res = yield call(api.auth.getUser, uid, token);
-                yield put(a.setAuth(res));
-                break;
+        try {
+            const action = yield take([
+                c.JOIN_TEAM,
+                c.CREATE_TEAM
+            ]);
+            console.log("taken");
+            const { token } = yield select(s.getAuth);
+            switch (action.type) {
+                case c.JOIN_TEAM: {
+                    const { number, pass } = action.payload;
+                    console.log(action.payload);
+                    const { id: uid } = yield select(s.getAuth);
+                    yield call(api.teams.join, number, pass, uid, token);
+                    const res = yield call(api.auth.getUser, uid, token);
+                    yield put(a.setAuth(res));
+                    yield put(push("/"));
+                    break;
+                }
+                case c.CREATE_TEAM: {
+                    const { name, number } = action.payload;
+                    console.log(action.payload);
+                    yield call(api.teams.create, name, number, token);
+                    const { id: uid } = yield select(s.getAuth);
+                    const res = yield call(api.auth.getUser, uid, token);
+                    yield put(a.setAuth(res));
+                    yield put(push("/"));
+                    break;
+                }
+                default:
+                    break;
             }
-            case c.CREATE_TEAM: {
-                const { name, number } = action.payload;
-                console.log(action.payload);
-                yield call(api.teams.create, name, number, token);
-                const { id: uid } = yield select(s.getAuth);
-                const res = yield call(api.auth.getUser, uid, token);
-                yield put(a.setAuth(res));
-                break;
-            }
-            default:
-                break;
+        }
+        catch (e) {
+            console.error(e);
         }
     }
 }
