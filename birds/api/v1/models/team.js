7d9c2fd6b95977dcc9b8de9c6325d2565ec0669e
Cole Scott <colescottsf@gmail.com>
2016-09-15 20:56:48 -0700

    Fix api to not be stupid (#51)
    * Fix up api
    
    * test
    
    * Fix error where userIsAdmin calles cb twice
    
    * Delete test file

--- birds/api/v1/models/team.js
+++ birds/api/v1/models/team.js
@@ -42,11 +42,15 @@ Team.statics.userIsAdmin = function(teamnumber, user, cb) {
             return cb(err);
         if (data.length <= 0)
             return cb("That team does not exist.");
-        data.users.forEach((usr) => {
-            if (usr.id == user.id)
-                return cb(null, usr.isAdmin);
+        var done = false;
+        data[ 0 ].users.forEach((usr) => {
+            if (usr.id == user.id) {
+                done = true;
+                cb(null, usr.isAdmin);
+            }
         });
-        return cb(null, false);
+        if(!done)
+            return cb(null, false);
     });
 };
 
@@ -82,6 +86,12 @@ Team.statics.exists = function(teamnumber, cb) {
         if (data.length <= 0)
             return cb(null, false);
         return cb(null, true);
+    });
+};
+
+Team.statics.addUser = function(teamnumber, user, isAdmin, cb) {
+    this.findOne({ teamnumber: teamnumber }).update({ $push: { "users": { id: user.id, isAdmin: isAdmin } } }, (err) => {
+        return cb(err);
     });
 };
 
--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-09-16T01:04:00.261Z",
+    "time": "2016-09-16T03:22:53.625Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
