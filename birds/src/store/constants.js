c73d15263cb2b3b514ec34fddecf30994f3762c0
Elias Lawson-Fox <eliaslfox@gmail.com>
2016-09-20 19:20:49 -0700

    named stuff (#90)

--- birds/src/store/constants.js
+++ birds/src/store/constants.js
@@ -2,12 +2,12 @@
 // Saga Actions
 export const ADD_USER = "ADD_USER";
 
-export const LOGIN_AUTH = "LOGIN_AUTH";
-export const LOGOUT_AUTH = "LOGOUT_AUTH";
-export const REGISTER_AUTH = "REGISTER_AUTH";
+export const AUTH_LOGIN = "AUTH_LOGIN";
+export const AUTH_LOGOUT = "AUTH_LOGOUT";
+export const AUTH_REGISTER = "AUTH_REGISTER";
 
-export const CREATE_TEAM = "CREATE_TEAM";
-export const JOIN_TEAM = "JOIN_TEAM";
+export const TEAM_CREATE = "TEAM_CREATE";
+export const TEAM_JOIN = "TEAM_JOIN";
 
 // Redux Store Actions
 export const REGISTER_FORM_SET = "REGISTER_FORM_SET";
@@ -19,5 +19,5 @@ export const LOGIN_FORM_RESET = "LOGIN_FORM_RESET";
 export const TEAM_FORM_SET = "TEAM_FORM_SET";
 export const TEAM_FORM_RESET = "TEAM_FORM_RESET";
 
-export const AUTH_SET = "AUTH_SET";
-export const AUTH_RESET = "AUTH_RESET";
+export const USER_SET = "USER_SET";
+export const USER_RESET = "USER_RESET";
