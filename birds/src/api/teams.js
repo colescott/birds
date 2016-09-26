b858f59c7351c7b9e6da581ef30af4896fc4ef71
Elias Lawson-Fox <eliaslfox@gmail.com>
2016-09-25 17:00:36 -0700

    fix linting errors

--- birds/src/api/teams.js
+++ birds/src/api/teams.js
@@ -4,13 +4,13 @@ const urlPrefix = (typeof URL_PREFIX !== "undefined" && URL_PREFIX) || "";
 
 const teams = {
     create: (name, number, token) => {
-        return xr.post(`${URL_PREFIX}/api/v1/teams`, { name, teamnumber: parseInt(number)}, {
+        return xr.post(`${URL_PREFIX}/api/v1/teams`, { name, teamnumber: parseInt(number) }, {
             headers: {
                 Authorization: `Bearer ${token}`,
                 "Content-Type": "application/json"
             }
         })
-            .then(res => res.data)
+            .then(res => res.data);
     },
     join: (number, password, uid, token) =>
         xr.put(`${URL_PREFIX}/api/v1/users/${uid}/jointeam`, { teamnumber: parseInt(number), password }, {
