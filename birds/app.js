bf9d658bfd4f8ab60c5cc7ad7832e60ac5dcc8e5
Cole Scott <colescottsf@gmail.com>
2016-09-28 15:10:26 -0700

    Add comments (ugh)

--- birds/app.js
+++ birds/app.js
@@ -14,7 +14,9 @@ mongoose.connect(dbConfig.url);
 const api = require("./api");
 
 const forceSSL = function(req, res, next) {
+    // This code checks the protocol from heroku proxy
     if (req.headers[ "x-forwarded-proto" ] !== "https") {
+        // Redirect with a 301
         return res.redirect(301, ["https://", req.get("Host"), req.url].join(""));
     }
     return next();
