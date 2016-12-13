5f5602e22cf869ef1680feecbb455ce9ee612d04
Cole Scott <colescottsf@gmail.com>
2016-12-13 15:09:55 -0800

    Add progress tracking to lesson list

--- birds/src/ui/lessonList/index.js
+++ birds/src/ui/lessonList/index.js
@@ -18,12 +18,43 @@ const styles = {
     }
 };
 
+const lessonProgressStyles = {
+    complete: {
+        color: "green",
+        textAlign: "right"
+    },
+    unstarted: {
+        color: "red",
+        textAlign: "right"
+    },
+    inprogress: {
+        color: "yellow",
+        textAlign: "right"
+    },
+    missingPrereqs: {
+        color: "darkred",
+        textAlign: "right"
+    }
+};
+
+const lessonProgressNames = {
+    complete: "Finished",
+    unstarted: "Not started",
+    inprogress: "In progress",
+    missingPrereqs: "Missing prerequisites"
+};
+
 const listLessonsInBranch = (props, branch) => {
     return branch.map(function(lesson, i) {
         return <div key={i}>
             <Link to={`/lesson/${lesson.id}`}>
                 <CardText>
-                    {lesson.title}
+                    <div style={{ lineHeight: 0 }}>
+                        {lesson.title}
+                        <div style={lessonProgressStyles[ lesson.progress ]}>
+                            {lessonProgressNames[ lesson.progress ]}
+                        </div>
+                    </div>
                 </CardText>
             </Link>
         </div>;
@@ -74,22 +105,54 @@ const mapStateToProps = (state) => ({
     get designBranch() {
         var branch = this.list.filter((lesson) => lesson.branch == "design");
         branch.name = "Design";
+        branch.map((lesson) => {
+            lesson.progress = this.lessonProgress[ lesson.id ] || "unstarted";
+            return lesson;
+        });
         return branch.sort();
     },
     get manufacturingBranch() {
         var branch = this.list.filter((lesson) => lesson.branch == "manufacturing");
         branch.name = "Manufacturing";
+        branch.map((lesson) => {
+            lesson.progress = this.lessonProgress[ lesson.id ] || "unstarted";
+            return lesson;
+        });
         return branch.sort();
     },
     get programmingBranch() {
         var branch = this.list.filter((lesson) => lesson.branch == "programming");
         branch.name = "Programming";
+        branch.map((lesson) => {
+            lesson.progress = this.lessonProgress[ lesson.id ] || "unstarted";
+            return lesson;
+        });
         return branch.sort();
     },
     get socialBranch() {
         var branch = this.list.filter((lesson) => lesson.branch == "social");
         branch.name = "Social";
+        branch.map((lesson) => {
+            lesson.progress = this.lessonProgress[ lesson.id ] || "unstarted";
+            return lesson;
+        });
         return branch.sort();
+    },
+    get lessonProgress() {
+        var progressArr = s.getUser(state).progress;
+        var progressObj = {};
+        for (const item in progressArr) {
+            progressObj[ progressArr[ item ].id ] = progressArr[ item ].state;
+        }
+        this.list.forEach((lesson) => {
+            for (const prereq in lesson.prerequisites) {
+                if (progressObj[ prereq.id ] != "complete") {
+                    progressObj[ lesson.id ] = "missingPrereqs";
+                    return;
+                }
+            }
+        });
+        return progressObj;
     },
 });
 
