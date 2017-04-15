25771874666e3d4a2b4d68abb382e10ef1f5988a
Cole Scott <colescottsf@gmail.com>
2017-04-14 22:58:17 -0700

    Fix lesson loading to properly use promises
    (I have no clue what aws was doing)

--- birds/api/v1/lessons.js
+++ birds/api/v1/lessons.js
@@ -13,18 +13,32 @@ const s3params = { Bucket: process.env.AWS_BUCKET };
 const s3bucket = new aws.S3({ params: s3params });
 
 const uploadLessonData = async (lesson, data) => {
-    let params = Object.assign({}, s3params, {
-        Key: `lessons/${lesson.id}`,
-        Body: data
+    return new Promise( (resolve, reject) => {
+        let params = {
+            Key: `lessons/${lesson.id}`,
+            Body: data
+        };
+        s3bucket.upload(params, (err) => {
+            if (err)
+                reject(err);
+            else
+                resolve();
+        });
     });
-    return s3bucket.upload(params);
 };
 
 const getLessonData = async (lesson) => {
-    let params = Object.assign({}, s3params, {
-        Key: `lessons/${lesson.id}`
+    return new Promise( (resolve, reject) => {
+        let params = {
+            Key: `lessons/${lesson.id}`
+        };
+        s3bucket.getObject(params, function(err, data) {
+            if (err)
+                reject(err);
+            else
+                resolve(data.Body.toString());
+        });
     });
-    return s3bucket.getObject(params);
 };
 
 /**
@@ -124,7 +138,7 @@ router.get("/:id", errorWrapper(async (req, res) => {
     if (!lesson)
         return util.error(res, "That lesson does not exist!", 400);
 
-    const data = await getLessonData({ id: lesson.id }).Body.toString();
+    const data = await getLessonData({ id: lesson.id });
 
     return util.data(res, util.sterilizeLessonWithData(lesson, data));
 }));
