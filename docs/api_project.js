f67aa68d51743080ac2927d08a110eddda221b41
Cole Scott <colescottsf@gmail.com>
2017-04-24 16:15:04 -0700

    Add api docs back in

---
+++ docs/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Birds api v1",
+  "version": "0.1.0",
+  "description": "Birds api version 1",
+  "title": "Birds api v1",
+  "url": "/api/v1",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-04-24T23:13:01.550Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
