629cc9f0f477d10111f0766ce499dc193f3969dd
Cole Scott <colescottsf@gmail.com>
2017-06-01 13:29:45 -0700

    Update docs to reflect 016854c

--- docs/api_project.js
+++ docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-04-24T23:13:01.550Z",
+    "time": "2017-06-01T20:28:57.085Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
